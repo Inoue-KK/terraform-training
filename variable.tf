03b9496eb369417620e4c6a8b8923f130079218d
inoue-kk <inoue-kk@users.noreply.github.com>
2022-10-11 17:04:36 +0900

    Added support for providers Azure and Google Cloud

--- variable.tf
+++ variable.tf
@@ -1,14 +1,28 @@
 variable "project_unique_id" {}
 
 variable "allowed_ipaddr_list" {
-  type = list(string)
-  default = [ "127.0.0.1/8" ]
+  type    = list(string)
+  default = ["127.0.0.1/8"]
 }
 
+# aws
 variable "base_dnsdomain" {
-  default = example.dev
+  default = "example.dev"
 }
 
-variable "aws_default_region" {}
+variable "aws_default_region" {
+  default = "us-east-1"
+}
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
+
+# azure
+variable "azure_default_location" {
+  default = "centralus"
+}
+
+# google
+variable "gcp_default_region" {
+  default = "us-central1"
+}
+variable "gcp_project_id" {}
